5e51081888f69c189a3f317abcc4cca63753e48d
Robin Kamboj <robinkamboj12@gmail.com>
2020-10-09 11:47:25 +0530

    uu

--- database/migrations/2020_10_09_061121_testing.php
+++ database/migrations/2020_10_09_061121_testing.php
@@ -17,6 +17,7 @@ class Testing extends Migration
         Schema::create('testing', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('fname');
             $table->string('lname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
